db963ad37daf44ca5d6d50abf74ed7671628a802
wiameanejjar <wiameanejjar@gmail.com>
2025-05-24 17:52:30 +0100

    commit

--- client-soap-java/src/main/java/ma/FS/Main.java
+++ client-soap-java/src/main/java/ma/FS/Main.java
@@ -7,6 +7,22 @@ public class Main {
     public static void main(String[] args) {
         BanqueService proxy = new BanqueWS().getBanqueServicePort();
         System.out.println(proxy.conversionEuroToDH(90));
+        Compte compte = proxy.getCompte(4);
+        System.out.println("----------------");
+        System.out.println(compte.getCode());
+        System.out.println(compte.getSolde());
+        System.out.println(compte.getDateCreation());
+
+
+
+        proxy.listCompte().forEach(cp->{
+            System.out.println("-------------------");
+            System.out.println(cp.getCode());
+            System.out.println(cp.getCode());
+            System.out.println(cp.getCode());
+
+        });
+
 
     }
 }
